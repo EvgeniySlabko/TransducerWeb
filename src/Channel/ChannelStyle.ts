154e0e10287bef9d504a57b4db4c07e6fd25b3d7
Kipelov99 <jeka949494@tut.by>
2022-04-06 16:26:05 +0300

    06.04

--- src/Channel/ChannelStyle.ts
+++ src/Channel/ChannelStyle.ts
@@ -1,6 +1,26 @@
+import { LayoutAxis, OhclData, PlotMarker } from 'plotly.js/lib/core';
 import { ColorsDefs } from '../Common/Colors';
 
 export class ChannelStyle{
-    LineColor = ColorsDefs.blue;
-    YAxeColor = ColorsDefs.green;
+    public traceStyle = {
+    name: 'yaxis',
+    marker:
+    {
+        color: ColorsDefs.black,
+        size: 0,
+    } as Partial<PlotMarker>,
+    line: {
+        width: 3,
+        dash: 'solid',
+        },
+            
+    } as Partial<OhclData>;
+
+    yAxeStyle = 
+    {
+    tickwidth: 2,
+    tickcolor: ColorsDefs.black,
+    color: ColorsDefs.black,
+    
+    } as Partial<LayoutAxis>
 }
