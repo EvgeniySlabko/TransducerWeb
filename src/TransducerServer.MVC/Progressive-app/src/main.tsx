3562ced80020fa1865aec2693e15bd2fcb0804d0
Kipelov99 <jeka949494@tut.by>
2022-07-20 15:40:31 +0300

    modift dir ignore.

--- src/TransducerServer.MVC/Progressive-app/src/main.tsx
+++ src/TransducerServer.MVC/Progressive-app/src/main.tsx
@@ -3,20 +3,15 @@ require('../css/cellStyles.css');
 require('../css/grow.css');
 require('../css/measureCell.css');
 
-require('../bootstrap-5/css/bootstrap.min.css');
-require('../bootstrap-5/js/bootstrap.bundle.min.js');
-
-require('../dist/uPlot.iife');
-require('../dist/uPlot.min.css');
-require('bootstrap/dist/css/bootstrap.min.css');
+require('../uPlot/uPlot.iife');
+require('../uPlot/uPlot.min.css');
 require('../src/initialStartup');
-import { notification } from 'antd';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './Components/App';
 import { recordController, sensorService, storage } from './initialStartup';
 import 'antd/dist/antd.css';
-//import Demo from './Components/SensorContainer';
 
 
 
